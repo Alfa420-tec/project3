551a38ad64295212df6139cfe6db59faf756db71
Alfa420-tec <malfarizky01090>
2024-01-16 14:37:20 +0700

    entertaiments

---
+++ entertaiments/config/global.php
@@ -0,0 +1,14 @@
+<?php
+
+    $_SITE_NAME		    = 'UBP Entertaiment';
+    $_SITE_DESC		    = 'UBP Entertai ment is a powerfull website';
+    $_SITE_NAME_LOGO    = 'EUBP';
+	$_SITE_DESC_EMAIL	= 'entertaiment@go.id';
+	$_SITE_DESC_PHONE	= '8123456789';
+    $_URL_DESC_FACEBOOK	= '';
+    $_URL_DESC_TWITTER	= '';
+    $_URL_DESC_INSTAGRAM= '';
+	$_SITE_CREDIT		= 'v1.0.0 - &copy 2023. All Right Reserved.';
+	$_QUERY_LIMIT		= 5;
+
+?>
